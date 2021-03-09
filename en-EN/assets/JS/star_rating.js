b07f0787ac66bafdbf5784079226b5a28c7de5d0
Mehdi GUERRIB <mehdi.guerrib.dz@viacesi.fr>
2021-03-09 11:45:51 +0100

    delali2

--- en-EN/assets/JS/star_rating.js
+++ en-EN/assets/JS/star_rating.js
@@ -2,19 +2,19 @@
 $('input[type = radio][name=rate]').on('change',function(){
     switch(this.value){
         case "1":
-            document.getElementById("ressenti").innerHTML= "NUL";
+            document.getElementById("ressenti").innerHTML= "BAD";
             break;
         case "2":
-            document.getElementById("ressenti").innerHTML= "ASSEZ BIEN";
+            document.getElementById("ressenti").innerHTML= "Pretty good";
             break;
         case "3":
-            document.getElementById("ressenti").innerHTML= "BIEN";
+            document.getElementById("ressenti").innerHTML= "GOOD";
             break;
         case "4":
             document.getElementById("ressenti").innerHTML= "EXCELLENT";
             break;
         case "5":
-            document.getElementById("ressenti").innerHTML= "PARFAIT";
+            document.getElementById("ressenti").innerHTML= "PERFECT";
             break;
     }
     $('#comm').show();  
